f26ea298f85f0b902d6a76d1ac5ed243be9976b8
GuiNobre1602 <guinobre1602@gmail.com>
2022-01-31 18:27:58 -0300

    No Toker

--- Python-Example/Ubi-dots-connect.py
+++ Python-Example/Ubi-dots-connect.py
@@ -1,10 +1,10 @@
 from ubidots import ApiClient
 
 # API KEY QUE É RETIRADA DO SITE
-api = ApiClient("BBFF-e93867912285537517c307dc821ec8bd09b")
+api = ApiClient("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 
 # PEGA O ID DO DEVICE QUE VAI SER UTILIZADO
-my_device = api.get_variable('61e1d9dd1d8472662f59d711')
+my_device = api.get_variable('XXXXXXXXXXXXXXXXX')
 
 last_value = my_device.get_values()
 print(last_value)
